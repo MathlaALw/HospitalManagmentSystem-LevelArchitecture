781e14e125ac6dabe2b9ddb49958b12d5f8deaa6
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-05 15:44:12 +0400

    Add Doctor class with validation properties

---
+++ Models/Doctor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagmentSystem_LevelArchitecture.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Specialization { get; set; }
+
+        [Required]
+        public string ContactNumber { get; set; }
+
+
+
+
+    }
+}
