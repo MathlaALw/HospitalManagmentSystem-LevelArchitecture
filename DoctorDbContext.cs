e4a5086574fe8c4200156e786bc42cf7999a7dc0
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-05 16:12:29 +0400

    Add DoctorDbContext for database configuration

---
+++ DoctorDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HospitalManagmentSystem_LevelArchitecture.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagmentSystem_LevelArchitecture
+{
+    public class DoctorDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-AEOLTE6;Initial Catalog=HospitalMSDB;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+
+
+    }
+}
