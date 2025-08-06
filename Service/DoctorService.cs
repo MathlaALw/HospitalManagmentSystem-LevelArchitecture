91a20b314fa6a4f0b249f04bbd520f141d92d289
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 14:19:26 +0400

    Implement IDoctorService interface and DoctorService

--- Service/DoctorService.cs
+++ Service/DoctorService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HospitalManagmentSystem_LevelArchitecture.Models;
+using HospitalManagmentSystem_LevelArchitecture.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace HospitalManagmentSystem_LevelArchitecture.Service
 {
-    public class DoctorService
+    public class DoctorService : IDoctorService
     {
+
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public void addDoctor(Doctor doctor)
+        {
+            var Doc = new Doctor
+            {
+                Name = doctor.Name,
+                Specialization = doctor.Specialization,
+                ContactNumber = doctor.ContactNumber,
+                Email = doctor.Email
+            };
+
+            _doctorRepository.AddDoctor(Doc);
+
+
+
+        }
+
+        public IEnumerable<Doctor> GetAllDoctors() {
+            return _doctorRepository.GetAllDoctors();
+        }
+
+
+
     }
 }
---
+++ Service/IDoctorService.cs
@@ -0,0 +1,10 @@
+﻿using HospitalManagmentSystem_LevelArchitecture.Models;
+
+namespace HospitalManagmentSystem_LevelArchitecture.Service
+{
+    public interface IDoctorService
+    {
+        void addDoctor(Doctor doctor);
+        IEnumerable<Doctor> GetAllDoctors();
+    }
+}
