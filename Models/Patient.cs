bf54bf6070ff4c3e2d727678a94f6259e1cdd6c8
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 08:38:53 +0400

    Add Patient class with validation properties

---
+++ Models/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagmentSystem_LevelArchitecture.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+       
+
+    }
+}
